33153bfb0ead317361b8d74ec9d7ea2d9aea4bee
em01 <github@em01.co.uk>
2018-01-22 16:58:57 +0700

    Add Error Boundary component.

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import '../styles/components/App.css';
 import Weather from '../containers/Weather';
+import ErrorBoundary from './common/ErrorBoundary';
 
 export class App extends Component {
   render() {
     return (
       <div className="App">
-        <Weather />
+        <ErrorBoundary>
+          <Weather />
+        </ErrorBoundary>
       </div>
     );
   }
